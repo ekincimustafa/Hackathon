79d9c05e0b5a32118ada2ee04d57a12f686d3f2a
Mustafa Ekinci <001mustafaekinci@gmail.com>
2026-05-19 18:43:42 +0300

    Sunucu linkleri değiştirildi.

--- frontend/app.js
+++ frontend/app.js
@@ -413,7 +413,7 @@ window.nextStep = function(stepNumber) {
             }
         }, 1500);
 
-        fetch('https://sanal-deneme.onrender.com/analyze', {
+        fetch('https://hackathon-a0bj.onrender.com/analyze', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(window.userData)
@@ -567,7 +567,7 @@ window.loadAlternative = function(styleText) {
         skinColorHex: window.userData.skinColorHex || "#000000"
     };
 
-    fetch('https://sanal-deneme.onrender.com/simulate_alternative', {
+    fetch('https://hackathon-a0bj.onrender.com/simulate_alternative', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(altData)
